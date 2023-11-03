84a712843274b04d3ad9b8cf5cb914c359a19ef9
Peter McEvoy <git@mcevoypeter.com>
2023-11-03 08:47:59 -0400

    Add routes

--- main.tf
+++ main.tf
@@ -23,3 +23,37 @@ resource "aws_apigatewayv2_stage" "this" {
   name        = each.value
   auto_deploy = true
 }
+
+resource "aws_lambda_permission" "apigw_trigger" {
+  for_each = {
+    for idx, route in var.routes : route.key => route
+  }
+
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = each.value.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.this.execution_arn}/*"
+}
+
+resource "aws_apigatewayv2_integration" "this" {
+  for_each = {
+    for idx, route in var.routes : route.key => route
+  }
+
+  api_id = aws_apigatewayv2_api.this.id
+  # see https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-api-integration-types.html
+  integration_type = "AWS_PROXY"
+  connection_type  = "INTERNET"
+  integration_uri  = each.value.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "this" {
+  for_each = {
+    for idx, route in var.routes : route.key => route
+  }
+
+  api_id    = aws_apigatewayv2_api.this.id
+  route_key = each.key
+  target    = "integrations/${aws_apigatewayv2_integration.this[each.key].id}"
+}
--- variables.tf
+++ variables.tf
@@ -18,3 +18,15 @@ variable "stages" {
   description = "Stage names. Each must be between 1 and 128 characters in length."
   type        = set(string)
 }
+
+variable "routes" {
+  description = "API routes"
+  type = set(object({
+    # Route key i.e. `GET /a/b/{id}`.
+    key = string
+    # Name of Lambda function that handles the route.
+    function_name = string
+    # Invocation ARN of the route-handling Lambda function.
+    invoke_arn = string
+  }))
+}
