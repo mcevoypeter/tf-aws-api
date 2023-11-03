53b843f439600458bfa87c62e44800f100912c7e
Peter McEvoy <git@mcevoypeter.com>
2023-11-03 10:04:30 -0400

    Configure logging and throttling for routes

--- main.tf
+++ main.tf
@@ -44,6 +44,22 @@ resource "aws_apigatewayv2_stage" "this" {
   api_id      = aws_apigatewayv2_api.this.id
   name        = each.value
   auto_deploy = true
+  default_route_settings {
+    logging_level          = "INFO"
+    throttling_burst_limit = 5000
+    throttling_rate_limit  = 10000
+  }
+  dynamic "route_settings" {
+    for_each = {
+      for idx, route in var.routes : route.key => route
+    }
+    content {
+      route_key              = route_settings.key
+      logging_level          = "INFO"
+      throttling_burst_limit = 5000
+      throttling_rate_limit  = 10000
+    }
+  }
 }
 
 resource "aws_lambda_permission" "apigw_trigger" {
