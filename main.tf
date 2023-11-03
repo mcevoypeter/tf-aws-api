43e3562bb3357678a1b650d8e23a4f16853504d0
Peter McEvoy <git@mcevoypeter.com>
2023-11-03 08:04:23 -0400

    Create API stages

--- main.tf
+++ main.tf
@@ -15,3 +15,11 @@ resource "aws_apigatewayv2_api" "this" {
   name          = var.name
   protocol_type = var.protocol_type
 }
+
+resource "aws_apigatewayv2_stage" "this" {
+  for_each = var.stages
+
+  api_id      = aws_apigatewayv2_api.this.id
+  name        = each.value
+  auto_deploy = true
+}
--- variables.tf
+++ variables.tf
@@ -13,3 +13,8 @@ variable "protocol_type" {
   description = "Type of API. Valid values: `HTTP`, `WEBSOCKET`."
   type        = string
 }
+
+variable "stages" {
+  description = "Stage names. Each must be between 1 and 128 characters in length."
+  type        = set(string)
+}
