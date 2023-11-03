35551d266b0d05cb6ac0fdd3aa7676fd641fbbf4
Peter McEvoy <git@mcevoypeter.com>
2023-11-03 13:49:01 -0400

    Ouptut API ARN

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "api_id" {
   description = "ID of API"
   value       = aws_apigatewayv2_api.this.id
 }
+
+output "api_arn" {
+  description = "ARN of API"
+  value       = aws_apigatewayv2_api.this.arn
+}
