a5a39f0ca095d5e532450ed2148076823d24e7e8
Peter McEvoy <git@mcevoypeter.com>
2023-11-20 10:52:37 -0700

    Output API execution_arn

--- output.tf
+++ output.tf
@@ -12,3 +12,8 @@ output "api_endpoint" {
   description = " URI of API"
   value       = aws_apigatewayv2_api.this.api_endpoint
 }
+
+output "api_execution_arn" {
+  description = "See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/apigatewayv2_api#execution_arn"
+  value       = aws_apigatewayv2_api.this.execution_arn
+}
