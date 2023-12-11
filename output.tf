f8a6fb253000e6eff3387d784a0aeeffde5574ea
Peter McEvoy <git@mcevoypeter.com>
2023-12-11 15:24:51 -0700

    Output Lambda IAM role info

--- output.tf
+++ output.tf
@@ -17,3 +17,24 @@ output "api_execution_arn" {
   description = "See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/apigatewayv2_api#execution_arn"
   value       = aws_apigatewayv2_api.this.execution_arn
 }
+
+output "lambda_role_ids" {
+  description = "IDs of Lambda IAM roles."
+  value = [
+    for role in aws_iam_role.this : role.id
+  ]
+}
+
+output "lambda_role_arns" {
+  description = "ARNs of Lambda IAM roles."
+  value = [
+    for role in aws_iam_role.this : role.arn
+  ]
+}
+
+output "lambda_role_names" {
+  description = "Names of Lambda IAM roles."
+  value = [
+    for role in aws_iam_role.this : role.name
+  ]
+}
