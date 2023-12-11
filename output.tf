05af644a0f5f72a35c0df52a019802e3309ee791
Peter McEvoy <git@mcevoypeter.com>
2023-12-11 15:31:01 -0700

    Tweak formatting in output descriptions

--- output.tf
+++ output.tf
@@ -3,22 +3,22 @@
 #
 
 output "api_id" {
-  description = "ID of API"
+  description = "ID of API."
   value       = aws_apigatewayv2_api.this.id
 }
 
 output "api_arn" {
-  description = "ARN of API"
+  description = "ARN of API."
   value       = aws_apigatewayv2_api.this.arn
 }
 
 output "api_endpoint" {
-  description = " URI of API"
+  description = "URI of API."
   value       = aws_apigatewayv2_api.this.api_endpoint
 }
 
 output "api_execution_arn" {
-  description = "See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/apigatewayv2_api#execution_arn"
+  description = "See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/apigatewayv2_api#execution_arn."
   value       = aws_apigatewayv2_api.this.execution_arn
 }
 
